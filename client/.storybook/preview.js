4c32f8f60cfcf92c35ac1c1e20f2efdc2b38a66e
tmdqls2257 <tmdqls2257@gmail.com>
2022-08-28 15:28:04 +0900

    🎉  begin project

---
+++ client/.storybook/preview.js
@@ -0,0 +1,19 @@
+// import "globals.css";
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//     // nextRouter: {
+//     //   Provider: RouterContext.Provider,
+//     // },
+//   },
+// };
+import "../styles/globals.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
---
+++ client/components/customForm/Form.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <form className="flex flex-col items-center gap-y-5">
+      <input
+        type="text"
+        className="rounded-full border-2 w-5/6 sm:w-96 h-12 px-3"
+      />
+      <div className="space-x-3">
+        <button
+          type="submit"
+          className="border-0 p-2 px-6 bg-slate-100 rounded-md"
+        >
+          Google Search
+        </button>
+        <button
+          type="submit"
+          className="border-0 p-2 px-6 bg-slate-100 rounded-md"
+        >
+          I&apos;m Feeling Lucky
+        </button>
+      </div>
+    </form>
+  );
+}
