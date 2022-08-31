be30add72b8906ec87259944625c0cd15f97071f
tmdqls2257 <tmdqls2257@gmail.com>
2022-08-31 23:38:20 +0900

    🔥 stories example

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -25,4 +25,22 @@ module.exports = {
   core: {
     builder: "@storybook/builder-webpack5",
   },
+  // webpackFinal: async (config) => {
+  //   // node_mules폴더와 styles 폴더 안의 모듈을 인식할 수 있게 함
+  //   config.resolve.modules = [
+  //     path.resolve(__dirname, ".."),
+  //     "node_modules",
+  //     "styles",
+  //   ];
+
+  //   // 절대 경로 설정
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     "@components": path.resolve(__dirname, "../components"),
+  //     "@pages": path.resolve(__dirname, "../pages"),
+  //     "@common": path.resolve(__dirname, "../common"),
+  //   };
+
+  //   return config;
+  // },
 };
