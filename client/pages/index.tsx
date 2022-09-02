78dea48c6cb036c679cb3da45dc71003f3830643
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-02 23:24:57 +0900

    💄 home stories

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import { HomeAnimation } from "../common/animation/animations";
 import Button, { LinkButton } from "../common/button/button";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <section className="body-font flex min-h-screen flex-col items-center justify-center text-gray-600">
       <div className="container mx-auto flex flex-col items-center px-5 py-24 md:flex-row">
