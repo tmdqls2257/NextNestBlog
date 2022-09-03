4ec66c49909d7f4a8ec275a912b83a5168cc9010
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-03 20:00:04 +0900

    ♻️ rename card to blogcard

---
+++ client/common/card/BlogCard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BlogModel } from "../../data/blogData";
+import { classNameJoiner } from "../../utils/className";
+import { useRouter } from "next/router";
+
+type CardProps = {
+  // children: React.ReactNode;
+  blogData: BlogModel;
+};
+
+export default function BlogCard({ blogData }: CardProps) {
+  const date = new Date(blogData.createdAt).toLocaleDateString().split(",");
+  // console.log(date.toLocaleString());
+  const router = useRouter();
+  const onclick = () => {
+    router.push(
+      {
+        pathname: `blogs/${blogData.id}`,
+        query: {
+          title: blogData.title,
+          content: blogData.contents,
+        },
+      },
+      `blogs/${blogData.id}`
+    );
+  };
+
+  return (
+    <li
+      className={classNameJoiner(
+        " cursor-pointer p-6 w-96 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      )}
+      onClick={onclick}
+    >
+      <h5 className="py-3">{blogData.title}</h5>
+      <p>{blogData.contents}</p>
+      {/* {children} */}
+
+      {/* <p>{Date.parse(blogData.createdAt)}</p> */}
+      <p className="flex flex-row-revers">{date[0]}</p>
+    </li>
+  );
+}
--- client/pages/blogs/index.tsx
+++ client/pages/blogs/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Layout from "../../layouts/layout";
 import { BlogData } from "../../data/blogData";
-import Card from "../../common/card/card";
+import BlogCard from "../../common/card/BlogCard";
 
 export default function Blogs() {
   return (
     <Layout>
       <ul className="flex gap-3">
         {BlogData.map((data, idx) => (
-          <Card blogData={data} key={idx}></Card>
+          <BlogCard blogData={data} key={idx}></BlogCard>
         ))}
       </ul>
     </Layout>
