de5b3c1bbedb25f9c0517f1aaf70760737deb45b
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-04 14:38:10 +0900

    💄 card css

--- client/common/card/card.tsx
+++ client/common/card/card.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BlogModel } from "../../data/blogData";
 import { classNameJoiner } from "../../utils/className";
-import { useRouter } from "next/router";
 
 type CardProps = {
   // children: React.ReactNode;
   children: React.ReactNode;
   onClick?: () => void;
+  style?: string;
 };
 
-export default function Card({ children, onClick }: CardProps) {
+export default function Card({ style, children, onClick }: CardProps) {
   return (
     <li
       className={classNameJoiner(
-        " cursor-pointer p-6 w-96 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        " cursor-pointer p-6 w-96 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700",
+        style
       )}
       onClick={onClick}
     >
