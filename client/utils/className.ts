78bb2faf242722841b35815cf6b42d923fc96964
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-04 14:40:32 +0900

    🌱 classNameJoiner

--- client/utils/className.ts
+++ client/utils/className.ts
@@ -1,3 +1,3 @@
-export function classNameJoiner(...classes: string[]) {
-  return classes.join(" ");
+export function classNameJoiner(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
 }
