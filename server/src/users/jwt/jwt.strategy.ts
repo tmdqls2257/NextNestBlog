3fdd28d291c92fa8e787413a18d23902ae9eddd3
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-05 18:09:54 +0900

    ✅ prettier

--- server/src/users/jwt/jwt.strategy.ts
+++ server/src/users/jwt/jwt.strategy.ts
@@ -1,21 +1,21 @@
-import { JwtPayload } from "./jwt.payload";
-import { UsersService } from "../users.service";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { PassportStrategy } from "@nestjs/passport";
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { jwtExtractorFromCookies } from "../../common/utils/jwtExtractorFromCookies";
-import { ConfigService } from "@nestjs/config";
+import { JwtPayload } from './jwt.payload';
+import { UsersService } from '../users.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { jwtExtractorFromCookies } from '../../common/utils/jwtExtractorFromCookies';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private readonly usersService: UsersService,
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
   ) {
     super({
       // cookie에 있는 jwt를 추출합니다.
       jwtFromRequest: ExtractJwt.fromExtractors([jwtExtractorFromCookies]),
-      secretOrKey: configService.get("SECRET_KEY"),
+      secretOrKey: configService.get('SECRET_KEY'),
       ignoreExpiration: false,
     });
   }
@@ -27,7 +27,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       if (user) {
         return user;
       } else {
-        throw new Error("해당하는 유저는 없습니다.");
+        throw new Error('해당하는 유저는 없습니다.');
       }
     } catch (error) {
       throw new UnauthorizedException(error);
