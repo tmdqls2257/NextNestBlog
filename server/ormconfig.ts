6e84ccc5e5cd0b8389625047e499bad3c703d0fa
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-18 13:34:00 +0900

    ♻️ process.env

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5434,
-  username: 'teogu',
-  password: '545255',
-  database: 'blog',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   logging: true,
   synchronize: false,
